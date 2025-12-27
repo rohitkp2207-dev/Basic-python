f4765b18e1cefb256e10262972005a4a3dfe3fbd
rohit6566332-dev <rohit6566332@gmail.com>
2025-12-27 23:36:33 +0530

    Add New

---
+++ print.py
@@ -0,0 +1,5 @@
+#This is the very basic concept in pyton to print any program.
+#We can print anything using the "print" keyword, The syntax for that is print()
+#For example
+
+print("Hello World !!")
