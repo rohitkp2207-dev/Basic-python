cb8c9b50e69779effe0c9ac7965a0435b4dfadf4
rohit6566332-dev <rohit6566332@gmail.com>
2025-12-29 07:15:26 +0530

    Added new file

---
+++ Variable.py
@@ -0,0 +1,20 @@
+#The variables are the basic but most important concept of any programming language.
+#In python we can assign the value for the variable by using "=" oprerator.
+#We can store the numerical value int he variable.
+#We can also print the value by using the variable.
+#for example
+
+A = "Hello, My name is Rohit."
+B = 2007    
+
+#printing the value of variable
+
+print(A)
+print(B)
+
+#Now we can do some operations on the variables
+
+Name = "Rohit"
+Surname = "Patil"
+Full_Name = Name + Surname
+print("The Full name is",Full_Name)
