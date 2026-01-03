5333b391045a4b2112def8b8cea628ea93d4386d
rohit6566332-dev <rohit6566332@gmail.com>
2026-01-03 18:38:33 +0530

    Added conditional statement

---
+++ conditional-statements.py
@@ -0,0 +1,12 @@
+# Conditional Statements : 
+# if-else:
+#syntax for if else statement is = if condition:
+                                          # statements
+
+# Write a program to check eligible for vote or not.
+
+Age = int(input("Enter your Age :"))
+if (Age>18):
+    print("You are egible for vote")
+else:
+    print("You are not eligible")    
